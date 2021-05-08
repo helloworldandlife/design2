fd9d2d30f51a81515984e6ba07820ce6b1c5789d
helloworldandlife <helloworldandlife@gmail.com>
2021-05-08 16:32:16 -0400

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,16 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+      <h1>DESIGN TORONTO</h1>
+      <p>Welcome to Design Toronto.</p>
+      <p>Now find amazing work here.</p>
+      <Link to="/page-2/">Watch the Video</Link>
+    </div>
+
+  </div>
+    
   </div>
 )
 
